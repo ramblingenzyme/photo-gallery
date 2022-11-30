ed433de97325d6585778c93f933c95e441e9d643
Satvik Sharma <satvik.sharma2@gmail.com>
2022-11-30 22:24:26 +1100

    Run prettier

--- build.mjs
+++ build.mjs
@@ -1,9 +1,9 @@
 import ejs from "ejs";
-import path from "path"
+import path from "path";
 import fs from "fs/promises";
 import { existsSync } from "fs";
-import url from 'url';
-import sizeOf from "image-size"
+import url from "url";
+import sizeOf from "image-size";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
@@ -12,27 +12,22 @@ const DIST_DIR = path.join(__dirname, "dist");
 
 const images = await fs.readdir(path.join(SRC_DIR, "images"));
 const imageInfo = images
-    .map(i => {
-        const dimensions = sizeOf(path.join(SRC_DIR, "images", i))
-        const isTall = dimensions.height > dimensions.width  
-        const isSquare = dimensions.height === dimensions.width  
- 
+  .map((i) => {
+    const dimensions = sizeOf(path.join(SRC_DIR, "images", i));
+    const isTall = dimensions.height > dimensions.width;
+    const isSquare = dimensions.height === dimensions.width;
 
-        return ({
-            src: i,
-            class: isTall 
-                ? "portrait" 
-                : isSquare
-                ? "square"
-                : undefined
-        });
-    })
-    .slice()
-    .reverse();
+    return {
+      src: i,
+      class: isTall ? "portrait" : isSquare ? "square" : undefined,
+    };
+  })
+  .slice()
+  .reverse();
 
 const content = await ejs.renderFile(
-    path.resolve(path.join(SRC_DIR, "template.ejs")),
-    { images: imageInfo }
+  path.resolve(path.join(SRC_DIR, "template.ejs")),
+  { images: imageInfo }
 );
 
 await fs.writeFile(path.join(SRC_DIR, "index.html"), content);
--- serve.mjs
+++ serve.mjs
@@ -1,14 +1,16 @@
 import express from "express";
 import path from "path";
-import url from 'url';
+import url from "url";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const app = express();
 const PATH = path.resolve(__dirname, "src");
 
-app.use(express.static(PATH, {
+app.use(
+  express.static(PATH, {
     extensions: ["html", "js", "css"],
-}));
+  })
+);
 
 app.listen("3000");
