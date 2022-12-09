193492a7cbbbd171f8b757a353d5a9e3d7c82d03
Satvik Sharma <satvik.sharma2@gmail.com>
2022-12-09 20:24:31 +1100

    Switch to pnpm and move site down into packages/

---
+++ packages/site/build.mjs
@@ -0,0 +1,38 @@
+import ejs from "ejs";
+import path from "path";
+import fs from "fs/promises";
+import { existsSync } from "fs";
+import url from "url";
+import sizeOf from "image-size";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const SRC_DIR = path.join(__dirname, "src");
+const DIST_DIR = path.join(__dirname, "dist");
+
+const images = await fs.readdir(path.join(SRC_DIR, "images"));
+const imageInfo = images
+  .map(i => {
+    const dimensions = sizeOf(path.join(SRC_DIR, "images", i));
+
+    // image orientations of 5 and above rotate so the dimensions are flipped :/
+    const isTall =
+      dimensions.orientation >= 5
+        ? dimensions.width > dimensions.height
+        : dimensions.height > dimensions.width;
+    const isSquare = dimensions.height === dimensions.width;
+
+    return {
+      src: i,
+      class: isTall ? "portrait" : isSquare ? "square" : undefined
+    };
+  })
+  .slice()
+  .reverse();
+
+const content = await ejs.renderFile(
+  path.resolve(path.join(SRC_DIR, "template.ejs")),
+  { images: imageInfo }
+);
+
+await fs.writeFile(path.join(SRC_DIR, "index.html"), content);
---
+++ packages/site/src/js/index.js
@@ -0,0 +1,66 @@
+const addCloseOnBackdrop = dialog => {
+  dialog.addEventListener("click", event => {
+    let rect = event.target.getBoundingClientRect();
+
+    const outsideX = rect.left > event.clientX || rect.right < event.clientX;
+    const outsideY = rect.top > event.clientY || rect.bottom < event.clientY;
+
+    function closeModal() {
+      dialog.close();
+      dialog.removeEventListener("transitionend", closeModal);
+    }
+
+    if (outsideX || outsideY) {
+      dialog.addEventListener("transitionend", closeModal);
+      dialog.classList.remove("in");
+    }
+  });
+};
+
+const addScrollOnClose = dialog => {
+  dialog.addEventListener("close", () => {
+    document.body.style.overflow = "visible";
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const dialog = document.getElementById("photo-popup");
+  const dialogContentWrapper = document.getElementById("dialog-wrapper");
+  const dialogImage = document.getElementById("dialog-img");
+  const dialogLink = document.getElementById("open-orig");
+
+  const imageWrappers = document.getElementsByClassName("photo-wrapper");
+
+  if (typeof dialog.showModal !== "function") {
+    dialog.hidden = true;
+    return;
+  }
+
+  addCloseOnBackdrop(dialog);
+  addScrollOnClose(dialog);
+
+  for (const imageWrapper of imageWrappers) {
+    imageWrapper.addEventListener("click", () => {
+      if (typeof dialog.showModal === "function") {
+        const nestedImg = Array.from(imageWrapper.children).find(
+          child => child.tagName === "IMG"
+        );
+
+        dialogImage.src = nestedImg.src;
+        dialogLink.href = nestedImg.src;
+
+        document.body.style.overflow = "hidden";
+        dialog.showModal();
+        dialog.classList.add("in");
+      }
+    });
+  }
+
+  const masonryFeatureElement = document.getElementById(
+    "masonry-feature-support"
+  );
+
+  if (CSS.supports("grid-template-rows", "masonry")) {
+    masonryFeatureElement.innerText = "hell yea, masonry layout";
+  }
+});
