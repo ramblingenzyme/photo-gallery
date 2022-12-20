b55f66f11bf93c27696c56d3cbcf65458ab6e806
Satvik Sharma <satvik.sharma2@gmail.com>
2022-12-20 21:01:36 +1100

    Refactor index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,68 +1,87 @@
-const addCloseOnBackdrop = dialog => {
-  dialog.addEventListener("click", event => {
-    let rect = event.target.getBoundingClientRect();
+const getDialog = () => document.getElementById("photo-popup");
+const getDialogImage = () => document.getElementById("dialog-img");
+const getDialogLink = () => document.getElementById("open-orig");
+const getBaseUrl = () =>
+  document.querySelector("[name=image-base-url]").content;
+const getMasonryFeatureElement = () =>
+  document.getElementById("masonry-feature-support");
 
-    const outsideX = rect.left > event.clientX || rect.right < event.clientX;
-    const outsideY = rect.top > event.clientY || rect.bottom < event.clientY;
+const closeModal = () => {
+  const dialog = getDialog();
 
-    function closeModal() {
-      dialog.close();
-      dialog.removeEventListener("transitionend", closeModal);
-    }
+  const closeImpl = () => {
+    dialog.close();
+    dialog.removeEventListener("transitionend", closeImpl);
+  };
 
-    if (outsideX || outsideY) {
-      dialog.addEventListener("transitionend", closeModal);
-      dialog.classList.remove("in");
-    }
-  });
+  dialog.addEventListener("transitionend", closeImpl);
+  dialog.classList.remove("in");
 };
 
-const addScrollOnClose = dialog => {
-  dialog.addEventListener("close", () => {
-    document.body.style.overflow = "visible";
-  });
+const openDialog = () => {
+  const dialog = getDialog();
+  dialog.showModal();
+  dialog.classList.add("in");
+};
+
+const hideOverflow = () => {
+  document.body.style.overflow = "hidden";
+};
+
+const showOverflow = () => {
+  document.body.style.overflow = "visible";
+};
+
+const closeOnBackdropClick = event => {
+  const dialog = getDialog();
+
+  let rect = event.target.getBoundingClientRect();
+
+  const outsideX = rect.left > event.clientX || rect.right < event.clientX;
+  const outsideY = rect.top > event.clientY || rect.bottom < event.clientY;
+
+  if (outsideX || outsideY) {
+    closeModal();
+  }
+};
+
+const openDialogForImage = imageWrapper => () => {
+  const nestedImg = Array.from(imageWrapper.children).find(
+    child => child.tagName === "IMG"
+  );
+
+  const dialogImage = getDialogImage();
+  const dialogLink = getDialogLink();
+  const baseUrl = getBaseUrl();
+
+  dialogImage.src = nestedImg.src;
+  dialogLink.href = `${baseUrl}/${nestedImg.dataset.imageKey}`;
+
+  hideOverflow();
+  openDialog();
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  const dialog = document.getElementById("photo-popup");
-  const dialogContentWrapper = document.getElementById("dialog-wrapper");
-  const dialogImage = document.getElementById("dialog-img");
-  const dialogLink = document.getElementById("open-orig");
-
-  const baseUrl = document.querySelector("[name=image-base-url]").content;
-
-  const imageWrappers = document.getElementsByClassName("photo-wrapper");
+  const dialog = getDialog();
 
   if (typeof dialog.showModal !== "function") {
     dialog.hidden = true;
     return;
   }
 
-  addCloseOnBackdrop(dialog);
-  addScrollOnClose(dialog);
+  dialog.addEventListener("click", closeOnBackdropClick);
+  dialog.addEventListener("cancel", event => {
+    event.preventDefault();
+    closeModal();
+  });
+  dialog.addEventListener("close", showOverflow);
 
+  const imageWrappers = document.getElementsByClassName("photo-wrapper");
   for (const imageWrapper of imageWrappers) {
-    imageWrapper.addEventListener("click", () => {
-      if (typeof dialog.showModal === "function") {
-        const nestedImg = Array.from(imageWrapper.children).find(
-          child => child.tagName === "IMG"
-        );
-
-        dialogImage.src = nestedImg.src;
-        dialogLink.href = `${baseUrl}/${nestedImg.dataset.imageKey}`;
-
-        document.body.style.overflow = "hidden";
-        dialog.showModal();
-        dialog.classList.add("in");
-      }
-    });
+    imageWrapper.addEventListener("click", openDialogForImage(imageWrapper));
   }
 
-  const masonryFeatureElement = document.getElementById(
-    "masonry-feature-support"
-  );
-
   if (CSS.supports("grid-template-rows", "masonry")) {
-    masonryFeatureElement.innerText = "hell yea, masonry layout";
+    getMasonryFeatureElement().innerText = "hell yea, masonry layout";
   }
 });
